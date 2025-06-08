9733d72d0d75bf894ccb804b457af30f8d379063
Sergey Koltsov <srg.koltsov@yandex.ru>
2025-06-08 16:39:04 +0300

    Update localizations imports for flutter 3.32.2

---
+++ lib/generated/l10n/app_localizations_en.dart
@@ -0,0 +1,19 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get name => 'Sergey Koltsov';
+
+  @override
+  String get company => 'Yandex';
+
+  @override
+  String get copied => 'Copied';
+}
---
+++ lib/generated/l10n/app_localizations_ru.dart
@@ -0,0 +1,19 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Russian (`ru`).
+class AppLocalizationsRu extends AppLocalizations {
+  AppLocalizationsRu([String locale = 'ru']) : super(locale);
+
+  @override
+  String get name => 'Сергей Кольцов';
+
+  @override
+  String get company => 'Яндекс';
+
+  @override
+  String get copied => 'Скопировано';
+}
